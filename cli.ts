f625991419b712f69dd2af8a62b761348c9ac660
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2024-05-14 22:02:56 +0700

    fix: show origin

--- cli.ts
+++ cli.ts
@@ -29,14 +29,13 @@ const workspacePath = process.cwd();
 console.log(workspacePath);
 const d = temporaryDirectory();
 process.chdir(d);
-$.cwd = d;
+// $.cwd = d;
 
 process.env.GH_TOKEN = core.getInput("token");
 process.env.GH_HOST = new URL(core.getInput("github_server_url")).host;
 
 await $`gh auth setup-git`;
 await $`git config --global --add safe.directory ${process.cwd()}`;
-await $`cd ${process.cwd()}`;
 
 await $`git init -b master`;
 await $`git remote add origin ${wikiGitURL}`;
@@ -86,6 +85,7 @@ if (core.getBooleanInput("preprocess")) {
 }
 
 await $`ls -al`;
+await $`ls -al ${process.cwd()}`;
 await $`git add -Av`;
 await $`git commit --allow-empty -m ${core.getInput("commit_message")}`;
 
